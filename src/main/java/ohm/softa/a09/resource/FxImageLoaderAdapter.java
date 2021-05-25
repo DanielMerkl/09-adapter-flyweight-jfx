9a20fd85caf1efbf72315ce4aed70cdaa665fc61
Daniel Merkl <daniel.merkl.dm@gmail.com>
2021-05-25 16:34:33 +0200

    assignment done

---
+++ src/main/java/ohm/softa/a09/resource/FxImageLoaderAdapter.java
@@ -0,0 +1,15 @@
+package ohm.softa.a09.resource;
+
+import javafx.scene.image.Image;
+
+public class FxImageLoaderAdapter {
+	private final ResourceLoader<Image> resourceLoader = new ResourceLoader<>(Image::new);
+
+	public Image loadImage(String resourcePath) {
+		return resourceLoader.loadResource(ClassLoader.getSystemClassLoader(), resourcePath);
+	}
+
+	public Image loadImage(ClassLoader resourceContext, String resourcePath) {
+		return resourceLoader.loadResource(resourceContext, resourcePath);
+	}
+}
